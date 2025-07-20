54d0a0ffe8c890e363a9aff733a3e8d68a0d0a6f
hyunseung <ardimento22@naver.com>
2025-07-20 19:30:49 +0900

    I/O 활용

---
+++ src/main/java/org/work/io/member/impl/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package org.work.io.member.impl;
+
+import org.work.io.member.Member;
+import org.work.io.member.MemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private final List<Member> members = new ArrayList<>();
+
+    @Override
+    public void add(Member member) {
+        members.add(member);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return members;
+    }
+}
